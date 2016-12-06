5cec7cecfde52733d0b0b5e8e1a973f9de90f19c
Luc Koppens <lhkoppens@gmail.com>
2016-12-06 18:33:54 +0100

    client side implementation

--- src/server/todo-server.js
+++ src/server/todo-server.js
@@ -53,8 +53,8 @@ function Label(name) {
 var t1 = new Todo('test', 'test', 'test', 'test', 'test' );
 var t2 = new Todo('test2', 'test2', 'test2', 'test2', 'test2' );
 
-todos.push(JSON.stringify(t1));
-todos.push(JSON.stringify(t2));
+todos.push(t1);
+todos.push(t2);
 
 //clients requests todos
 app.get("/todos", function (req, res) {
