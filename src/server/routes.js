08584efd7645b7c33e6f7fc9d16d52e82656ebd3
Calvin Quan-Luan Nhieu <cqnhieu@uwaterloo.ca>
2016-12-17 21:02:24 +0100

    actually modularize server code

--- src/server/routes.js
+++ src/server/routes.js
@@ -5,77 +5,15 @@ var http = require("http");
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
 var config = require('./config.js');
+var db = require('./db.js');
 
-var connection = mysql.createConnection({
-	host	: 'localhost',
-	port	: 3306,
-	user	: 'root',
-	password: config.mysql_password,
-	database: config.database
-});
-connection.connect(function(err) {
-  if (err) {
-    console.error('<<<Error connecting to Db>>>');
-    return;
-  }
-  console.log('>>>>DB Connection established');
-});
-
+db.connect();
 //settings and default vars
 var port = 3000;
 var app = express();
 app.use(bodyParser());
 app.use(express.static(__dirname + "/client"));
 http.createServer(app).listen(port);
-var todosJSON = [];
-
-// from todo.js
-var lists = [];
-var todos = [];
-var labels = {};
-
-// Todo object
-function Todo(desc, dueDate, labels, notes, priority) {
-  this.id = -1;
-  this.description = desc;
-  this.createDate = '';
-  this.dueDate = dueDate;
-  this.labels = labels;
-  this.notes = notes;
-  this.priority = priority;
-  this.completed = false;
-  this.deleted = false;
-  this.isEditing = false;
-};
-
-// TodoList object
-function TodoList(listName) {
-  this.id = -1;
-  this.name = listName;
-  this.todos = [];
-  this.sortByDate = function() {};
-  this.sortByPriority = function() {};
-};
-
-// Label object
-function Label(name) {
-  this.id = -1;
-  this.name = name;
-  this.count = 1;
-};
-
-//data
-function data(todos, lists) {
-  this.todos = todos;
-  this.lists = lists;
-};
-
-//add some test todos
-todos.push(new Todo('test', 'test', '', 'test', 'test'));
-todos.push(new Todo('test2', 'test2', '', 'test', 'test'));
-var newList = new TodoList('Eerste')
-lists.push(newList);
-
 
 //ENDPOINTS
 //for debugging
@@ -88,7 +26,7 @@ app.get("/todos", function (req, res) {
 //Client requests todo lists
 app.post("/getLists", function (req, res) {
   //get UserID
-  connection.query('select * from todo_list', function(err, result) {
+  db.query('select * from todo_list', function(err, result) {
     if (err) {
       console.error(err);
     }
@@ -100,7 +38,7 @@ app.post("/getLists", function (req, res) {
 //Client requests todo lists
 app.post("/getTodos", function (req, res) {
   //get UserID
-  connection.query('select * from todo_item', function(err, result) {
+  db.query('select * from todo_item', function(err, result) {
     if (err) {
       console.error(err);
     }
@@ -111,7 +49,7 @@ app.post("/getTodos", function (req, res) {
 
 app.post("/getLabels", function (req, res) {
   //get UserID
-  connection.query('select * from label', function(err, result) {
+  db.query('select * from label', function(err, result) {
     if (err) {
       console.error(err);
     }
@@ -135,7 +73,7 @@ app.post("/addTodo", function (req, res) {
     priority: req.body.priority,
     completed: req.body.completed
   };
-  connection.query('insert into todo_item set ?', todo, function(err, result) {
+  db.query('insert into todo_item set ?', todo, function(err, result) {
     if (err) {
       console.error(err);
     }
@@ -175,7 +113,7 @@ app.post("/updateTodo", function (req, res) {
     priority: req.body.data.priority,
     completed: req.body.data.completed
   };
-  connection.query('update todo_item set ? where id = ' + id, todo, function(err, result) {
+  db.query('update todo_item set ? where id = ' + id, todo, function(err, result) {
     if (err) {
       console.error(err);
     }
@@ -187,7 +125,7 @@ app.post("/updateTodo", function (req, res) {
 
 //Client deletes Todo
 app.post("/deleteTodo", function (req, res) {
-  connection.query('delete from todo_item where id = ' + req.body.id, function(err, result) {
+  db.query('delete from todo_item where id = ' + req.body.id, function(err, result) {
     if (err) {
       console.error(err);
     }
@@ -202,7 +140,7 @@ app.post("/deleteTodo", function (req, res) {
 //List Todoslist from given user
 //user id
 app.post("/listListUser", function (req, res) {
-  connection.query('SELECT * FROM todo.todo_list WHERE owner_id =  ' + req.body.id, function(err, result) {
+  db.query('SELECT * FROM todo.todo_list WHERE owner_id =  ' + req.body.id, function(err, result) {
     if (err) {
       console.error(err);
     }
@@ -214,7 +152,7 @@ app.post("/listListUser", function (req, res) {
 //List Todolist for given Todo
 // list id
 app.post("/listTodoList", function (req, res) {
-  connection.query('SELECT * FROM todo.todo_item WHERE todo_list_id = ' + req.body.id, function(err, result) {
+  db.query('SELECT * FROM todo.todo_item WHERE todo_list_id = ' + req.body.id, function(err, result) {
     if (err) {
       console.error(err);
     }
@@ -226,7 +164,7 @@ app.post("/listTodoList", function (req, res) {
 //Paginated list Todolist for given Todo
 //list id, lowLim, hiLim
 app.post("/paglistTodoList", function (req, res) {
-  connection.query('SELECT * FROM todo.todo_item WHERE todo_list_id = ' +  req.body.id +' LIMIT ' + req.body.lowLim + ', ' + req.body.hiLim, function(err, result) {
+  db.query('SELECT * FROM todo.todo_item WHERE todo_list_id = ' +  req.body.id +' LIMIT ' + req.body.lowLim + ', ' + req.body.hiLim, function(err, result) {
     if (err) {
       console.error(err);
     }
